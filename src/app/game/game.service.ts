ad9ceea6b12467b7122a48e53b70066790965f3d
Muhammad Salman <muhdsalm44@gmail.com>
2023-12-23 12:18:08 +0500

    Fix some syntax errors

--- src/app/game/game.service.ts
+++ src/app/game/game.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core'
-import { Game } from '../../../Crickin/game';
+import { Game, TeamNumber } from '../../../Crickin/game';
+import { Batsman } from '../../../Crickin';
 
 @Injectable({
   providedIn: 'root',
 })
-export class FirstScreen {
+export class GameService {
 
   private game: Game
 
   team1Name: string
   team2Name: string
 
-  playerNamesTeam1: [string]
-  playerNamesTeam2: [string]
+  playerNamesTeam1: string[]
+  playerNamesTeam2: string[]
 
-  overs: number
+  overs: number = 6
 
   battingTeam: boolean
 
@@ -23,8 +24,44 @@ export class FirstScreen {
   bowler: number
 
 
-  constructor() {
+  constructor() {}
+
+  setTeamNames(team1: string, team2: string) {
+    this.team1Name, this.team2Name = team1, team2
+  }
+
+  setPlayerName(team: TeamNumber, players: string[]) {
+    if (team) { // TeamNumber.Two
+      this.playerNamesTeam2 = players
+    } else {
+      this.playerNamesTeam1 = players
+    }
+  }
+
+  setOvers(overs: number) {
+    this.overs = overs
+  }
+
+  setBattingTeam(team: TeamNumber) {
+    this.battingTeam = !!team
+  }
+
+  setBatsmen(batsmanNumber1: number, batsmanNumber2: number) {
+    this.batsmanNumber1 = batsmanNumber1, this.batsmanNumber2 = batsmanNumber2
+  }
+
+  setBowler(bowlerNumber: number) {
+    this.bowler = bowlerNumber
+  }
+
+  createGame() {
     this.game = new Game()
+    this.game.setOvers(this.overs)
+    this.game.setTeamNames(this.team1Name, this.team2Name)
+    this.game.setPlayerNames(this.playerNamesTeam1, this.playerNamesTeam2)
+    this.game.setBattingTeam(TeamNumber[this.battingTeam ? "One" : "Two"])
+    this.game.setBowlingTeam(TeamNumber[!this.battingTeam ? "One" : "Two"])
+    this.game.selectPlayers(this.batsmanNumber1, this.batsmanNumber2, this.bowler)
   }
 
 
